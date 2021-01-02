63ae8af49e63f006b6e7a422ca698949e6de3dc7
palmbeach05 <davmcleod87@gmail.com>
2021-01-01 22:42:06 -0500

    Cleanup
    Remove unnecessary bloat.
    Update vendor setup script.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,3 @@
 LOCAL_PATH := device/samsung/j5xltecmcc
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/omni_j5xltecmcc.mk \
-	$(LOCAL_DIR)/rr_j5xltecmcc.mk \
-	$(LOCAL_DIR)/lineage_j5xltecmcc.mk
+	$(LOCAL_DIR)/omni_j5xltecmcc.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,6 @@
 for i in eng userdebug user; do
-	add_lunch_combo lineage_j5xltecmcc-${i}
+	add_lunch_combo omni_j5xltecmcc-${i}
+	
+export LC_ALL=C
+export ALLOW_MISSING_DEPENDENCIES=true
 done
