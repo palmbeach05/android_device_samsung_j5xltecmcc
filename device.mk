42d132ff184b95e325608babd994bc5be19e59bf
palmbeach05 <5945226+palmbeach05@users.noreply.github.com>
2026-06-09 08:37:44 -0400

    j5xltecmcc: Cleanup
    Overlay was removed a while ago, somehow this line was missed. Also,
    RIL values are not needed for TWRP, and omni.dependencies are present
    in msm8916-common, j5x-common, and in each variant's directory. Let's
    get rid of the redundant duplicity to eliminate any dependency
    conflicts, synchronization headaches, and redundant manifest parsing
    during workspace syncs.

--- device.mk
+++ device.mk
@@ -5,9 +5,3 @@ $(call inherit-product-if-exists, vendor/samsung/j5xltecmcc/j5xltecmcc-vendor.mk
 $(call inherit-product, device/samsung/j5x-common/device-common.mk)
 
 LOCAL_PATH := device/samsung/j5xltecmcc
-
-# System properties
--include $(LOCAL_PATH)/system_prop.mk
-
-# Common overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
