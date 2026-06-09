05e0bebae91c21e7af3c52272820b7d5b82588de
palmbeach05 <5945226+palmbeach05@users.noreply.github.com>
2026-06-09 08:02:16 -0400

    j5xltecmcc: Move system_prop values to j5x-common
    The values that were in system_prop.mk are common amongst all of the
    j5 2016 devices. It would be more appropriate to have them moved to
    j5x-common. Also, drop zram property as this is disabled by default.

--- system_prop.mk
+++ system_prop.mk
@@ -1,12 +1,3 @@
-# CABL
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.qualcomm.cabl=2
-
-# RAM
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.config.low_ram=false \
-	ro.config.zram=false
-
 # RIL
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	telephony.lteOnCdmaDevice=0 \
